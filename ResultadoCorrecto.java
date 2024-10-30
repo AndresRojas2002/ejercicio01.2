cfdb04741498b128ab9ba0a1f7de0a37eeb5e495
luis andres rojas acevedo <rojasandres2002.24@gmail.com>
2024-10-29 20:03:01 -0500

    ejec1

---
+++ ResultadoCorrecto.java
@@ -0,0 +1,18 @@
+/**
+ * ResultadoCorrecto
+ */
+public class ResultadoCorrecto {
+public static void main(String[] args) {
+    var operacion = 6 / 2 * (1+2) ;
+    System.out.println(operacion);
+}
+
+/* Para resolver esta expresión, seguimos el orden de operaciones:
+
+1. Paréntesis: 1+2 = 3
+2. luego divide 6 entre 2: 6÷2 = 3
+y por ultimo multiplica 3 por 3: 3 * 3 = 9
+        
+*/
+    
+}
